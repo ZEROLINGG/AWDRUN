7073137bdb2e269a5bf13dbf3fb535461c91d647
ZEROLINGG <bcsz8833221@gmail.com>
2025-09-23 12:50:50 +0800

    添加核心攻击模块到应用

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 ﻿from dataclasses import dataclass
 
 from config import Config
+from get_flag.attack import Attack
 from kv import Kv
 from submit_flag.send import Send
 
@@ -16,6 +17,9 @@ class AwdRun:
         self.kv = Kv()  # 键值对存储
         self.config = Config()  # 自动化配置
         self.send = Send(self.config.flag_ip)   # flag提交工具
+        self.attack = Attack(self.kv)   # 基础攻击工具
+
+        self.send.set_headers(self.config.auth_headers)
         
         
         
